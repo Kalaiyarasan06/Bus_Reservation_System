85c32c4efa64dc179e1de8a0e90d3606a1f02790
Kalaiyarasan06 <130954271+Kalaiyarasan06@users.noreply.github.com>
2025-08-29 11:28:00 +0530

    hided the Db password

--- src/BusResrv/DbConnection.java
+++ src/BusResrv/DbConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DbConnection {
     private static String url="jdbc:mysql://localhost:3306/busresrv";
     private static String username="root";
-    private static String password="kalaivelu25";
+    private static String password="XXXXXXXXXX";
 
     public static Connection  getConnection() throws SQLException {
         return DriverManager.getConnection(url,username,password);
